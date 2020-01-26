c244400f3813c4d7a994e0a00cca34d134a293ca
Ivan <vanya71310@gmail.com>
2020-01-26 21:50:32 +0300

    Добавляет отрисоку элемента Picture

---
+++ js/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var PICTURE_DESCRIPTIONS = ['Описание1', 'Описание2', 'Описание3', 'Описание4', 'Описание5', 'Описание6', 'Описание7'];
+var COMMENT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var COMMENT_NAMES = ['Артем', 'Иван', 'Андрей', 'Сергей', 'Денис', 'Петр', 'Павел', 'Вероника'];
+var pictureTemplate = document.querySelector('#picture').content.firstElementChild;
+
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getRandomArrValue = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var generateComments = function (cnt) {
+  var comments = [];
+  for (var i = 0; i < cnt; i++) {
+    var comment = {};
+    comment.avatar = 'img/avatar-' + getRandomInt(1, 6) + '.svg';
+    comment.message = getRandomArrValue(COMMENT_MESSAGES);
+    comment.name = getRandomArrValue(COMMENT_NAMES);
+    comments.push(comment);
+  }
+  return comments;
+};
+
+var generatePhotoObjects = function (cnt) {
+  var photos = [];
+  for (var i = 0; i < cnt; i++) {
+    var photo = {};
+    photo.url = 'photos/' + (i + 1) + '.jpg';
+    photo.description = getRandomArrValue(PICTURE_DESCRIPTIONS);
+    photo.likes = getRandomInt(15, 200);
+    photo.comments = generateComments(getRandomInt(1, 10));
+    photos.push(photo);
+  }
+  return photos;
+};
+
+var renderPicture = function (pictureElement) {
+  var pictureNode = pictureTemplate.cloneNode(true);
+  pictureNode.querySelector('.picture__img').setAttribute('src', pictureElement.url);
+  pictureNode.querySelector('.picture__likes').innerText = pictureElement.likes;
+  pictureNode.querySelector('.picture__comments').innerText = pictureElement.comments.length;
+  return pictureNode;
+};
+
+var generatePictureFragment = function (arr) {
+  var fragmentPicture = document.createDocumentFragment();
+  for (var i = 0; i < arr.length; i++) {
+    fragmentPicture.appendChild(renderPicture(arr[i]));
+  }
+  return fragmentPicture;
+};
+
+var appendPicture = function () {
+  document.querySelector('.pictures').appendChild(generatePictureFragment(generatePhotoObjects(25)));
+};
+
+appendPicture();
