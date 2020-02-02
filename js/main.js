bbf6c976b9084812f560f7915aa8bf5377eb31e1
Ivan <vanya71310@gmail.com>
2020-02-02 20:09:59 +0300

    Добавляет выполнение задания

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@
 var PICTURE_DESCRIPTIONS = ['Описание1', 'Описание2', 'Описание3', 'Описание4', 'Описание5', 'Описание6', 'Описание7'];
 var COMMENT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var COMMENT_NAMES = ['Артем', 'Иван', 'Андрей', 'Сергей', 'Денис', 'Петр', 'Павел', 'Вероника'];
+var ESCAPE_BTN = 'Escape';
 var pictureTemplate = document.querySelector('#picture').content.firstElementChild;
 var bigPicture = document.querySelector('.big-picture');
+var uploadFile = document.getElementById('upload-file');
+var editImgForm = document.querySelector('.img-upload__overlay');
+var closeImgFormBtn = editImgForm.querySelector('.img-upload__cancel');
+var effectLevelPin = editImgForm.querySelector('.effect-level__pin');
+var effectsList = editImgForm.querySelector('.effects__list');
+var hashtags = editImgForm.querySelector('.text__hashtags');
+var imgUploadBtn = editImgForm.querySelector('.img-upload__submit');
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -92,5 +100,72 @@ var showPicture = function (pictureElement) {
 };
 
 appendPicture(generatePictureFragment(photos));
-showPicture(photos[0]);
+// showPicture(photos[0]);
+
+var openImgForm = function () {
+  editImgForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
+
+var closeImgForm = function () {
+  editImgForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  editImgForm.value = '';
+};
+
+var closeImgFormKeyEscHandler = function (evt) {
+  var isHashtagsFocused = (document.activeElement === hashtags);
+  if (evt.key === ESCAPE_BTN && !isHashtagsFocused) {
+    closeImgForm();
+  }
+};
+
+uploadFile.addEventListener('change', function () {
+  openImgForm();
+  document.addEventListener('keydown', closeImgFormKeyEscHandler);
+});
+
+closeImgFormBtn.addEventListener('click', closeImgForm);
+
+effectLevelPin.addEventListener('mouseup', function (evt) {
+  var totalHeight = getComputedStyle(evt.target.parentNode).width;
+  var pinPos = evt.target.offsetLeft;
+  var effectLevel = pinPos / parseInt(totalHeight, 10);
+});
+
+var checkHashtags = function (value) {
+  if (value === '' || value === null) {
+    return false;
+  }
+  var hashArray = value.split(' ');
+  if (hashArray.length > 5) {
+    return 'Допускается не более 5 хештегов';
+  }
+
+  for (var i = 0; i < hashArray.length; i++) {
+    var checkValue = hashArray[i].match(/^#[\wа-яА-я]+/) || [];
+    var counter = 0;
+    if (hashArray[i] !== checkValue[0]) {
+      return 'Хештег номер ' + (i + 1) + ' имеет неверный формат';
+    }
+    for (var j = 0; j < hashArray.length; j++) {
+      if (hashArray[i].toLowerCase() === hashArray[j].toLowerCase()) {
+        counter++;
+      }
+    }
+    if (counter > 1) {
+      return 'Имеются одинаковые хештеги';
+    }
+  }
+  return false;
+};
+
+
+imgUploadBtn.addEventListener('click', function () {
+  if (checkHashtags(hashtags.value)) {
+    hashtags.setCustomValidity(checkHashtags(hashtags.value));
+  } else {
+    hashtags.setCustomValidity('');
+  }
+});
 
