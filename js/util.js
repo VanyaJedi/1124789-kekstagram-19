2600c7c47280bcc215d83444ffe6a1a1890b47d7
Ivan <vanya71310@gmail.com>
2020-02-28 20:39:21 +0300

    Добавляет корректировки

--- js/util.js
+++ js/util.js
@@ -1,43 +1,11 @@
 'use strict';
 
 (function () {
-  var PICTURE_DESCRIPTIONS = ['Описание1', 'Описание2', 'Описание3', 'Описание4', 'Описание5', 'Описание6', 'Описание7'];
-  var COMMENT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  var COMMENT_NAMES = ['Артем', 'Иван', 'Андрей', 'Сергей', 'Денис', 'Петр', 'Павел', 'Вероника'];
   var ESCAPE_BTN = 'Escape';
   var ENTER_BTN = 'Enter';
 
   var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  };
-
-  var getRandomArrValue = function (arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  };
-
-  var generateComments = function (cnt, messages, names) {
-    var comments = [];
-    for (var i = 0; i < cnt; i++) {
-      var comment = {};
-      comment.avatar = 'img/avatar-' + getRandomInt(1, 6) + '.svg';
-      comment.message = getRandomArrValue(messages);
-      comment.name = getRandomArrValue(names);
-      comments.push(comment);
-    }
-    return comments;
-  };
-
-  var generatePhotoObjects = function (cnt, messages, names, descriptions) {
-    var photos = [];
-    for (var i = 0; i < cnt; i++) {
-      var photo = {};
-      photo.url = 'photos/' + (i + 1) + '.jpg';
-      photo.description = getRandomArrValue(descriptions);
-      photo.likes = getRandomInt(15, 200);
-      photo.comments = generateComments(getRandomInt(1, 10), messages, names);
-      photos.push(photo);
-    }
-    return photos;
   };
 
   var checkHashtags = function (value) {
@@ -76,14 +44,11 @@
     }
   };
 
-  var photos = generatePhotoObjects(25, COMMENT_MESSAGES, COMMENT_NAMES, PICTURE_DESCRIPTIONS);
 
   window.util = {
-    photos: photos,
     ESCAPE_BTN: ESCAPE_BTN,
     ENTER_BTN: ENTER_BTN,
     getRandomInt: getRandomInt,
-    getRandomArrValue: getRandomArrValue,
     checkHashtags: checkHashtags,
     removeAllClasses: removeAllClasses
   };
